c0b41b3977b81f793cfa566493c3ed65f6731101
neuronaddict <22725031+NeuronAddict@users.noreply.github.com>
2024-10-27 22:08:38 +0100

    bye script

---
+++ bye.py
@@ -0,0 +1,10 @@
+import time
+
+if __name__ == '__main__':
+
+    for thanks in [' aux dévelopeurs de pytorch', ' aux créateurs de fastai et pour leur excellent tutoriel', ' à jetbrains pour son excellent IDE (+ vim pour les acharnés)'
+                   ' à vous qui regardez cette vidéo', ' pour votre curiosité', 'et vous pouvez vous abonner pour être notifié des prochaines '
+                                                       'vidéos']:
+        print('#' * 50)
+        print(f'Merci {thanks}\n')
+        time.sleep(4)
